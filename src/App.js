6a4a913a872ad1e4a88bfa6c827ecdbe9ae74681
Dan Hounshell <dan@danhounshell.com>
2018-10-25 11:00:32 -0400

    Added tabbed interface

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import './App.css';
 import GroupsView from './GroupsView';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
 
 class App extends Component {
   render() {
@@ -9,9 +11,28 @@ class App extends Component {
       <MuiThemeProvider>
         <div className="App">
           <header className="App-header">
-            <h1 className="App-title">Home Lighting</h1>
+            <h1 className="App-title">Home Automation</h1>
           </header>
-          <GroupsView />
+          <Tabs defaultIndex={0}>
+            <TabList>
+              <Tab>Lighting</Tab>
+              <Tab>Switches</Tab>
+              <Tab>Security</Tab>
+              <Tab disabled>Other</Tab>
+            </TabList>
+            <TabPanel>
+              <GroupsView />
+            </TabPanel>
+            <TabPanel>
+              <h2>Coming Soon</h2>
+            </TabPanel>
+            <TabPanel>
+              <h2>Coming Soon</h2>
+            </TabPanel>
+            <TabPanel>
+              <h2>Coming Soon</h2>
+            </TabPanel>
+          </Tabs>          
         </div>
       </MuiThemeProvider>
     );
